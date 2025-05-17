f0c5af3d6ff4d651daf27c55065211c31123aa87
TinaKovalova <kovalevatkv@gmail.com>
2025-05-17 20:37:35 +0300

    reviews

--- js/swiper.js
+++ js/swiper.js
@@ -10,7 +10,25 @@ const gallerySwiper = new Swiper(".gallery__swiper .swiper", {
   slidesPerView: "auto",
   spaceBetween: 20,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".gallery__swiper .swiper-button-next",
+    prevEl: ".gallery__swiper .swiper-button-prev",
+  },
+});
+
+
+const reviewsSwiper = new Swiper(".reviews__swiper .swiper", {
+  //   loop: true,
+  speed: 2500,
+  grabCursor: true,
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false,
+  //     pauseOnMouseEnter: true,
+  //   },
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".reviews__swiper .swiper-button-next",
+    prevEl: ".reviews__swiper .swiper-button-prev",
   },
 });
